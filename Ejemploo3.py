42d47f43920b7d5ba62e9e0762a4123011480aad
JoseBacilio <74934503@continental.edu.pe>
2024-09-09 20:10:03 -0500

    Refactorizó área del rectángulo

--- Ejemploo3.py
+++ Ejemploo3.py
@@ -1,7 +1,7 @@
 class Figura:
-    def f(self, a, b):
-        result = a * b
-        return result
+    def areaDelRectángulo(self, Altura, Base):
+        area = Altura * Base
+        return area
 
 
     # Función para calcular el área de un triángulo
@@ -14,7 +14,7 @@ class Figura:
     def main(self):
         x = 4
         y = 6
-        rect_area = self.f(x, y)
+        rect_area = self.areaDelRectángulo(x, y)
         print("Área del rectángulo:", rect_area)
 
         base = 5
