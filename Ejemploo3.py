71aec72c8f86a035f0f55590474b38c72163f6fa
JoseBacilio <74934503@continental.edu.pe>
2024-09-09 20:04:33 -0500

    Orientado a POO

--- Ejemploo3.py
+++ Ejemploo3.py
@@ -1,27 +1,25 @@
-class Figura:
 # Función para calcular el área de un rectángulo
-
-def f(self,a, b):
+def f(a, b):
     result = a * b
     return result
 
 # Función para calcular el área de un triángulo
-def g(self,b, h):
+def g(b, h):
     r = 0.5 * b * h
     return r
 
 # Función principal
-def main(self):
-    x = 4
-    y = 6
-    rect_area = self.f(x, y)
+def main():
+    # Entrada de valores para el rectángulo
+    x = float(input("Ingrese el ancho del rectángulo: "))
+    y = float(input("Ingrese la altura del rectángulo: "))
+    rect_area = f(x, y)
     print("Área del rectángulo:", rect_area)
 
-    base = 5
-    altura = 8
-    tri_area = self.g(base, altura)
-    print("Área del triángulo:", tri_area)
+    # Entrada de valores para el triángulo
+    base = float(input("Ingrese la base del triángulo: "))
+    altura = float(input("Ingrese la altura del triángulo: "))
+    tri_area = g(base, altura)
+    print("Área del triángulo es :", tri_area)
 
-if __name__"__main__":
-    figura = Figura()
-    figura.main()
+main()
